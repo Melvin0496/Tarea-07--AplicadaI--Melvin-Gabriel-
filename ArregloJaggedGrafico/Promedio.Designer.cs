d0de2cab7358802d83c6e6dc5c5bf0c4072d3fe4
Melvin0496 <melvingabriel65@gmail.com>
2015-09-21 12:03:35 -0400

    esta es la tarea 7

---
+++ ArregloJaggedGrafico/Promedio.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ArregloJaggedGrafico
+{
+    partial class Promedio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextotextBox = new System.Windows.Forms.TextBox();
+            this.Presionabutton = new System.Windows.Forms.Button();
+            this.Clickbutton = new System.Windows.Forms.Button();
+            this.NumeroslistBox = new System.Windows.Forms.ListBox();
+            this.SumatextBox = new System.Windows.Forms.TextBox();
+            this.PromediotextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MaximatextBox = new System.Windows.Forms.TextBox();
+            this.MinimatextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TextotextBox
+            // 
+            this.TextotextBox.Location = new System.Drawing.Point(82, 55);
+            this.TextotextBox.Name = "TextotextBox";
+            this.TextotextBox.Size = new System.Drawing.Size(100, 20);
+            this.TextotextBox.TabIndex = 0;
+            // 
+            // Presionabutton
+            // 
+            this.Presionabutton.Location = new System.Drawing.Point(203, 226);
+            this.Presionabutton.Name = "Presionabutton";
+            this.Presionabutton.Size = new System.Drawing.Size(75, 23);
+            this.Presionabutton.TabIndex = 1;
+            this.Presionabutton.Text = "Presiona";
+            this.Presionabutton.UseVisualStyleBackColor = true;
+            this.Presionabutton.Click += new System.EventHandler(this.Presionabutton_Click);
+            // 
+            // Clickbutton
+            // 
+            this.Clickbutton.Location = new System.Drawing.Point(324, 226);
+            this.Clickbutton.Name = "Clickbutton";
+            this.Clickbutton.Size = new System.Drawing.Size(75, 23);
+            this.Clickbutton.TabIndex = 2;
+            this.Clickbutton.Text = "Clickea";
+            this.Clickbutton.UseVisualStyleBackColor = true;
+            this.Clickbutton.Click += new System.EventHandler(this.Clickbutton_Click);
+            // 
+            // NumeroslistBox
+            // 
+            this.NumeroslistBox.FormattingEnabled = true;
+            this.NumeroslistBox.Items.AddRange(new object[] {
+            "Numeros"});
+            this.NumeroslistBox.Location = new System.Drawing.Point(219, 55);
+            this.NumeroslistBox.Name = "NumeroslistBox";
+            this.NumeroslistBox.Size = new System.Drawing.Size(191, 147);
+            this.NumeroslistBox.TabIndex = 3;
+            // 
+            // SumatextBox
+            // 
+            this.SumatextBox.Location = new System.Drawing.Point(82, 81);
+            this.SumatextBox.Name = "SumatextBox";
+            this.SumatextBox.ReadOnly = true;
+            this.SumatextBox.Size = new System.Drawing.Size(100, 20);
+            this.SumatextBox.TabIndex = 4;
+            // 
+            // PromediotextBox
+            // 
+            this.PromediotextBox.Location = new System.Drawing.Point(82, 112);
+            this.PromediotextBox.Name = "PromediotextBox";
+            this.PromediotextBox.ReadOnly = true;
+            this.PromediotextBox.Size = new System.Drawing.Size(100, 20);
+            this.PromediotextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Numeros:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Suma:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Promedio:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Maxima:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Minima:";
+            // 
+            // MaximatextBox
+            // 
+            this.MaximatextBox.Location = new System.Drawing.Point(82, 146);
+            this.MaximatextBox.Name = "MaximatextBox";
+            this.MaximatextBox.ReadOnly = true;
+            this.MaximatextBox.Size = new System.Drawing.Size(100, 20);
+            this.MaximatextBox.TabIndex = 11;
+            // 
+            // MinimatextBox
+            // 
+            this.MinimatextBox.Location = new System.Drawing.Point(82, 183);
+            this.MinimatextBox.Name = "MinimatextBox";
+            this.MinimatextBox.ReadOnly = true;
+            this.MinimatextBox.Size = new System.Drawing.Size(100, 20);
+            this.MinimatextBox.TabIndex = 12;
+            // 
+            // Promedio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 261);
+            this.Controls.Add(this.MinimatextBox);
+            this.Controls.Add(this.MaximatextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PromediotextBox);
+            this.Controls.Add(this.SumatextBox);
+            this.Controls.Add(this.NumeroslistBox);
+            this.Controls.Add(this.Clickbutton);
+            this.Controls.Add(this.Presionabutton);
+            this.Controls.Add(this.TextotextBox);
+            this.Name = "Promedio";
+            this.Text = "Ventana";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextotextBox;
+        private System.Windows.Forms.Button Presionabutton;
+        private System.Windows.Forms.Button Clickbutton;
+        private System.Windows.Forms.ListBox NumeroslistBox;
+        private System.Windows.Forms.TextBox SumatextBox;
+        private System.Windows.Forms.TextBox PromediotextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox MaximatextBox;
+        private System.Windows.Forms.TextBox MinimatextBox;
+    }
+}
+
---
+++ ArregloJaggedGrafico/Promedio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArregloJaggedGrafico
+{
+    public partial class Promedio : Form
+    {
+        ArrayList Arreglo;
+        public Promedio()
+        {
+            InitializeComponent();
+            Arreglo = new ArrayList();
+        }
+        int i = 0;
+        float promedio = 0.0f,suma = 0.0f;
+        int valor;
+        private void Presionabutton_Click(object sender, EventArgs e)
+        {
+            valor = Convert.ToInt32(TextotextBox.Text);
+            Arreglo.Add(valor);
+            i++;
+            suma += valor;
+            promedio = suma / i;
+            TextotextBox.Text = "";
+        }
+        
+        private void Clickbutton_Click(object sender, EventArgs e)
+        {
+            int i,x = 0;
+            for(i = 0;i < Arreglo.Count;i++)
+            {
+                Arreglo.Sort();
+                NumeroslistBox.Items.Add(Arreglo[i]);
+            }
+            SumatextBox.Text = Convert.ToString(suma);
+            PromediotextBox.Text = Convert.ToString(promedio);
+            MinimatextBox.Text = Convert.ToString(Arreglo[0]);
+           // MaximatextBox.Text = Convert.ToString(Arreglo[1]);
+           
+        }
+    }
+}
